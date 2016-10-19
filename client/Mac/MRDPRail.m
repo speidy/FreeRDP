7c889b585ceae60f7a797771f070e1f61a63b508
Jay Sorg <jay.sorg@gmail.com>
2016-10-19 11:37:17 -0700

    Mac: work on RAIL

--- client/Mac/MRDPRail.m
+++ client/Mac/MRDPRail.m
@@ -10,20 +10,246 @@
 
 #import <Foundation/Foundation.h>
 #import "MRDPRail.h"
+#import "MRDPWindow.h"
 
-
-BOOL mac_window_create(rdpContext* context, WINDOW_ORDER_INFO* orderInfo, WINDOW_STATE_ORDER* window_state)
+BOOL mac_window_common(rdpContext* context, WINDOW_ORDER_INFO* orderInfo, WINDOW_STATE_ORDER* windowState)
 {
-    return TRUE;
-}
+    mfAppWindow* appWindow = NULL;
+    UINT32 fieldFlags = orderInfo->fieldFlags;
+    mfContext* mfc = (mfContext*) context;
+    BOOL position_or_size_updated = FALSE;
 
-BOOL mac_window_update(rdpContext* context, WINDOW_ORDER_INFO* orderInfo, WINDOW_STATE_ORDER* window_state)
-{
+    if (fieldFlags & WINDOW_ORDER_STATE_NEW)
+    {
+        appWindow = (mfAppWindow*) calloc(1, sizeof(mfAppWindow));
+        if (appWindow == NULL)
+        {
+            return FALSE;
+        }
+        appWindow->mfc = mfc;
+        appWindow->windowId = orderInfo->windowId;
+        appWindow->dwStyle = windowState->style;
+        appWindow->dwExStyle = windowState->extendedStyle;
+        appWindow->x = appWindow->windowOffsetX = windowState->windowOffsetX;
+        appWindow->y = appWindow->windowOffsetY = windowState->windowOffsetY;
+        appWindow->width = appWindow->windowWidth = windowState->windowWidth;
+        appWindow->height = appWindow->windowHeight = windowState->windowHeight;
+        /* Ensure window always gets a window title */
+        if (fieldFlags & WINDOW_ORDER_FIELD_TITLE)
+        {
+            char* title = NULL;
+            if (windowState->titleInfo.length == 0)
+            {
+                if (!(title = _strdup("")))
+                {
+                    //WLog_ERR(TAG, "failed to duplicate empty window title string");
+                    /* error handled below */
+                }
+            }
+            else if (ConvertFromUnicode(CP_UTF8, 0, (WCHAR*) windowState->titleInfo.string,
+                                        windowState->titleInfo.length / 2, &title, 0, NULL, NULL) < 1)
+            {
+                //WLog_ERR(TAG, "failed to convert window title");
+                /* error handled below */
+            }
+            appWindow->title = title;
+        }
+        else
+        {
+            if (!(appWindow->title = _strdup("RdpRailWindow")))
+            {
+                //WLog_ERR(TAG, "failed to duplicate default window title string");
+            }
+        }
+        if (!appWindow->title)
+        {
+            free(appWindow);
+            return FALSE;
+        }
+        HashTable_Add(mfc->railWindows, (void*) (UINT_PTR) orderInfo->windowId, (void*) appWindow);
+        mf_AppWindowInit(mfc, appWindow);
+    }
+    else
+    {
+        appWindow = (mfAppWindow*) HashTable_GetItemValue(mfc->railWindows, (void*) (UINT_PTR) orderInfo->windowId);
+    }
+    if (appWindow == NULL)
+    {
+        return FALSE;
+    }
+    /* Keep track of any position/size update so that we can force a refresh of the window */
+    if ((fieldFlags & WINDOW_ORDER_FIELD_WND_OFFSET) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_WND_SIZE) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_CLIENT_AREA_OFFSET) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_CLIENT_AREA_SIZE) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_WND_CLIENT_DELTA) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_VIS_OFFSET) ||
+        (fieldFlags & WINDOW_ORDER_FIELD_VISIBILITY))
+    {
+        position_or_size_updated = TRUE;
+    }
+    /* Update Parameters */
+    if (fieldFlags & WINDOW_ORDER_FIELD_WND_OFFSET)
+    {
+        appWindow->windowOffsetX = windowState->windowOffsetX;
+        appWindow->windowOffsetY = windowState->windowOffsetY;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_WND_SIZE)
+    {
+        appWindow->windowWidth = windowState->windowWidth;
+        appWindow->windowHeight = windowState->windowHeight;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_OWNER)
+    {
+        appWindow->ownerWindowId = windowState->ownerWindowId;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_STYLE)
+    {
+        appWindow->dwStyle = windowState->style;
+        appWindow->dwExStyle = windowState->extendedStyle;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_SHOW)
+    {
+        appWindow->showState = windowState->showState;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_TITLE)
+    {
+        char* title = NULL;
+        if (windowState->titleInfo.length == 0)
+        {
+            if (!(title = _strdup("")))
+            {
+                //WLog_ERR(TAG, "failed to duplicate empty window title string");
+                return FALSE;
+            }
+        }
+        else if (ConvertFromUnicode(CP_UTF8, 0, (WCHAR*) windowState->titleInfo.string,
+                                    windowState->titleInfo.length / 2, &title, 0, NULL, NULL) < 1)
+        {
+            //WLog_ERR(TAG, "failed to convert window title");
+            return FALSE;
+        }
+        free(appWindow->title);
+        appWindow->title = title;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_CLIENT_AREA_OFFSET)
+    {
+        appWindow->clientOffsetX = windowState->clientOffsetX;
+        appWindow->clientOffsetY = windowState->clientOffsetY;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_CLIENT_AREA_SIZE)
+    {
+        appWindow->clientAreaWidth = windowState->clientAreaWidth;
+        appWindow->clientAreaHeight = windowState->clientAreaHeight;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_WND_CLIENT_DELTA)
+    {
+        appWindow->windowClientDeltaX = windowState->windowClientDeltaX;
+        appWindow->windowClientDeltaY = windowState->windowClientDeltaY;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_WND_RECTS)
+    {
+        if (appWindow->windowRects)
+        {
+            free(appWindow->windowRects);
+            appWindow->windowRects = NULL;
+        }
+        appWindow->numWindowRects = windowState->numWindowRects;
+        if (appWindow->numWindowRects)
+        {
+            appWindow->windowRects = (RECTANGLE_16*) calloc(appWindow->numWindowRects, sizeof(RECTANGLE_16));
+            if (appWindow->windowRects == NULL)
+            {
+                return FALSE;
+            }
+            CopyMemory(appWindow->windowRects, windowState->windowRects,
+                       appWindow->numWindowRects * sizeof(RECTANGLE_16));
+        }
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_VIS_OFFSET)
+    {
+        appWindow->visibleOffsetX = windowState->visibleOffsetX;
+        appWindow->visibleOffsetY = windowState->visibleOffsetY;
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_VISIBILITY)
+    {
+        if (appWindow->visibilityRects)
+        {
+            free(appWindow->visibilityRects);
+            appWindow->visibilityRects = NULL;
+        }
+        appWindow->numVisibilityRects = windowState->numVisibilityRects;
+        if (appWindow->numVisibilityRects)
+        {
+            appWindow->visibilityRects = (RECTANGLE_16*) calloc(appWindow->numVisibilityRects, sizeof(RECTANGLE_16));
+            if (appWindow->visibilityRects == NULL)
+            {
+                return FALSE;
+            }
+            CopyMemory(appWindow->visibilityRects, windowState->visibilityRects,
+                       appWindow->numVisibilityRects * sizeof(RECTANGLE_16));
+        }
+    }
+    /* Update Window */
+    if (fieldFlags & WINDOW_ORDER_FIELD_STYLE)
+    {
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_SHOW)
+    {
+        mf_ShowWindow(mfc, appWindow, appWindow->showState);
+    }
+    if (fieldFlags & WINDOW_ORDER_FIELD_TITLE)
+    {
+        if (appWindow->title != NULL)
+        {
+            mf_SetWindowText(mfc, appWindow, appWindow->title);
+        }
+    }
+    if (position_or_size_updated)
+    {
+        UINT32 visibilityRectsOffsetX =
+                (appWindow->visibleOffsetX - (appWindow->clientOffsetX - appWindow->windowClientDeltaX));
+        UINT32 visibilityRectsOffsetY =
+                (appWindow->visibleOffsetY - (appWindow->clientOffsetY - appWindow->windowClientDeltaY));
+        /*
+         * The rail server like to set the window to a small size when it is minimized even though it is hidden
+         * in some cases this can cause the window not to restore back to its original size. Therefore we don't
+         * update our local window when that rail window state is minimized
+         */
+        if (appWindow->rail_state != WINDOW_SHOW_MINIMIZED)
+        {
+            /* Redraw window area if already in the correct position */
+            if (appWindow->x == appWindow->windowOffsetX &&
+                appWindow->y == appWindow->windowOffsetY &&
+                appWindow->width == appWindow->windowWidth &&
+                appWindow->height == appWindow->windowHeight)
+            {
+                mf_UpdateWindowArea(mfc, appWindow, 0, 0, appWindow->windowWidth, appWindow->windowHeight);
+            }
+            else
+            {
+                mf_MoveWindow(mfc, appWindow, appWindow->windowOffsetX, appWindow->windowOffsetY,
+                              appWindow->windowWidth, appWindow->windowHeight);
+            }
+            mf_SetWindowVisibilityRects(mfc, appWindow, visibilityRectsOffsetX, visibilityRectsOffsetY,
+                                        appWindow->visibilityRects, appWindow->numVisibilityRects);
+        }
+    }
     return TRUE;
 }
 
 BOOL mac_window_delete(rdpContext* context, WINDOW_ORDER_INFO* orderInfo)
 {
+    mfAppWindow* appWindow = NULL;
+    mfContext* mfc = (mfContext*) context;
+
+    appWindow = (mfAppWindow*) HashTable_GetItemValue(mfc->railWindows, (void*) (UINT_PTR) orderInfo->windowId);
+    if (appWindow == NULL)
+    {
+        return TRUE;
+    }
+    HashTable_Remove(mfc->railWindows, (void*) (UINT_PTR) orderInfo->windowId);
+    mf_DestroyWindow(mfc, appWindow);
     return TRUE;
 }
 
@@ -81,22 +307,22 @@ static UINT mac_rail_server_handshake(RailClientContext* context, RAIL_HANDSHAKE
     RAIL_LANGBAR_INFO_ORDER langBarInfo;
     mfContext* mfc = (mfContext*) context->custom;
     rdpSettings* settings = mfc->context.settings;
-    
+
     ZeroMemory(&clientHandshake, sizeof(clientHandshake));
     clientHandshake.buildNumber = 0x00001DB0;
     context->ClientHandshake(context, &clientHandshake);
-    
+
     ZeroMemory(&clientStatus, sizeof(RAIL_CLIENT_STATUS_ORDER));
     clientStatus.flags = RAIL_CLIENTSTATUS_ALLOWLOCALMOVESIZE;
     context->ClientInformation(context, &clientStatus);
-    
+
     if (settings->RemoteAppLanguageBarSupported)
     {
         ZeroMemory(&langBarInfo, sizeof(langBarInfo));
         langBarInfo.languageBarStatus = 0x00000008; /* TF_SFT_HIDDEN */
         context->ClientLanguageBarInfo(context, &langBarInfo);
     }
-    
+
     ZeroMemory(&sysparam, sizeof(RAIL_SYSPARAM_ORDER));
     sysparam.params = 0;
     sysparam.params |= SPI_MASK_SET_HIGH_CONTRAST;
@@ -118,13 +344,13 @@ static UINT mac_rail_server_handshake(RailClientContext* context, RAIL_HANDSHAKE
     sysparam.workArea.bottom = settings->DesktopHeight;
     sysparam.dragFullWindows = FALSE;
     context->ClientSystemParam(context, &sysparam);
-    
+
     ZeroMemory(&exec, sizeof(RAIL_EXEC_ORDER));
     exec.RemoteApplicationProgram = settings->RemoteApplicationProgram;
     exec.RemoteApplicationWorkingDir = settings->ShellWorkingDirectory;
     exec.RemoteApplicationArguments = settings->RemoteApplicationCmdLine;
     context->ClientExecute(context, &exec);
-    
+
     return CHANNEL_RC_OK;
 }
 
@@ -156,13 +382,13 @@ static UINT mac_rail_server_get_appid_response(RailClientContext* context, RAIL_
 void mac_rail_init(mfContext* mfc, RailClientContext* rail)
 {
     rdpWindowUpdate* window;
-    
+
     rail->custom = (void*) mfc;
     mfc->rail = rail;
     window = mfc->context.update->window;
     window->context = &mfc->context;
-    window->WindowCreate = mac_window_create;
-    window->WindowUpdate = mac_window_update;
+    window->WindowCreate = mac_window_common;
+    window->WindowUpdate = mac_window_common;
     window->WindowDelete = mac_window_delete;
     window->WindowIcon = mac_window_icon;
     window->WindowCachedIcon = mac_window_cached_icon;
@@ -171,7 +397,7 @@ void mac_rail_init(mfContext* mfc, RailClientContext* rail)
     window->NotifyIconDelete = mac_rail_notify_icon_delete;
     window->MonitoredDesktop = mac_rail_monitored_desktop;
     window->NonMonitoredDesktop = mac_rail_non_monitored_desktop;
-    
+
     rail->ServerExecuteResult = mac_rail_server_execute_result;
     rail->ServerSystemParam = mac_rail_server_system_param;
     rail->ServerHandshake = mac_rail_server_handshake;
---
+++ client/Mac/MRDPWindow.m
@@ -0,0 +1,38 @@
+
+#import <Foundation/Foundation.h>
+
+#import "O1RDPRail.h"
+#import "O1RDPSession_private.h"
+
+int mf_AppWindowInit(mfContext* mfc, mfAppWindow* appWindow)
+{
+    return 1;
+}
+
+void mf_DestroyWindow(mfContext* mfc, mfAppWindow* appWindow)
+{
+}
+
+void mf_MoveWindow(mfContext* mfc, mfAppWindow* appWindow,
+                   int x, int y, int width, int height)
+{
+}
+
+void mf_UpdateWindowArea(mfContext* mfc, mfAppWindow* appWindow,
+                         int x, int y, int width, int height)
+{
+}
+
+void mf_SetWindowVisibilityRects(mfContext* mfc, mfAppWindow* appWindow,
+                                 UINT32 rectsOffsetX, UINT32 rectsOffsetY,
+                                 RECTANGLE_16* rects, int nrects)
+{
+}
+
+void mf_SetWindowText(mfContext* mfc, mfAppWindow* appWindow, char* name)
+{
+}
+
+void mf_ShowWindow(mfContext* mfc, mfAppWindow* appWindow, BYTE state)
+{
+}
